30ac1b6ecf0a5f0b4bb4101a483b0aa104c28643
propardus <48132183+propardus@users.noreply.github.com>
2019-03-02 19:57:07 +0300

    Добавили: выбор рандомного слова

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from random import choice
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('otvet.html')
+    if not 'gamer' in session:
+        f = open('slova.txt', 'r')
+        words = f.read().lower()
+        f.close()
+        words = words.split('\n')
+        word = choice(words)
+        xword = '■'*len(word)
+    return render_template('otvet.html', x_word=xword, answer=word)
 
 
 @app.route('/sendme', methods=['POST'])
 def sendme():
     ...
 
+app.secret_key = 'isduf*&&(*(*&@jhhje893w92803834e'
 app.run()
