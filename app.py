1f45356666cd92b3f34824f8f17b609c44dbedf8
propardus <48132183+propardus@users.noreply.github.com>
2019-03-02 20:16:49 +0300

    Доделано: до базова виселица

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect
 from random import choice
 
 app = Flask(__name__)
@@ -12,12 +12,30 @@ def index():
         words = words.split('\n')
         word = choice(words)
         xword = '■'*len(word)
+        session['word'] = word
+        session['xword'] = xword
+        session['gamer'] = True
+    else:
+        xword = session['xword']
+        word = session['word']
+    if not '■' in xword:
+        session.pop('gamer')
     return render_template('otvet.html', x_word=xword, answer=word)
 
 
-@app.route('/sendme', methods=['POST'])
+@app.route('/send', methods=['POST'])
 def sendme():
-    ...
+    char = request.form['char']
+    xword = session['xword']
+    word = session['word']
+    new_xword = ''
+    for i in range(len(word)):
+        if word[i] == char:
+            new_xword += char
+        else:
+            new_xword += xword[i]
+    session['xword'] = new_xword
+    return redirect('/')
 
 app.secret_key = 'isduf*&&(*(*&@jhhje893w92803834e'
 app.run()
