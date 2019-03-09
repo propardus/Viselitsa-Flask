4c5202a7a196695ea3497796fa7f5cbc74103da6
propardus <48132183+propardus@users.noreply.github.com>
2019-03-09 19:26:19 +0300

    доделал до совершенства дз

--- app.py
+++ app.py
@@ -3,26 +3,40 @@ from random import choice
 
 app = Flask(__name__)
 
+
+@app.before_first_request
+def before_request():
+    session['gamer']=False
+    session['nepravda'] = 0
+    session['pravda'] = 0
+
 @app.route('/')
 def index():
     global lif
-    if not 'gamer' in session:
+    lif = 10
+    if session['gamer']==False:
         f = open('slova.txt', 'r')
         words = f.read().lower()
         f.close()
-        lif=10
         words = words.split('\n')
         word = choice(words)
         xword = '■'*len(word)
         session['word'] = word
         session['xword'] = xword
+        session['lif']=lif
         session['gamer'] = True
+        session['bukvi']=['']
+
     else:
         xword = session['xword']
         word = session['word']
+        lif = session['lif']
     if not '■' in xword:
-        session.pop('gamer')
-    return render_template('otvet.html', x_word=xword, answer=word,live=lif)
+        session['gamer'] = False
+        session['pravda'] = session['pravda'] + 1
+        return render_template('pobandproi.html', ot='победили')
+    else:
+        return render_template('otvet.html', x_word=xword, answer=word,live=lif)
 
 
 @app.route('/send', methods=['POST'])
@@ -32,15 +46,40 @@ def sendme():
     xword = session['xword']
     word = session['word']
     new_xword = ''
-    for i in range(len(word)):
-        if word[i] == char:
-            new_xword += char
+    print(session['bukvi'])
+    if char not in session['bukvi'] :
+        session['bukvi'].append(char)
+        for i in range(len(word)):
+            if word[i] == char:
+                new_xword += char
+            else:
+                new_xword += xword[i]
+        if new_xword==xword:
+            lif = lif-1
+        # return '{}, {}, {}'.format(session['word'], session['xword'], request.form['char'])
+        if lif <= 0:
+            session['nepravda'] = session['nepravda'] + 1
+            session['gamer']=False
+            return render_template('pobandproi.html',ot='проиграли')
         else:
-            lif=lif-1
-            new_xword += xword[i]
-    session['xword'] = new_xword
-    session['live'] = lif
+            session['xword'] = new_xword
+            session['lif'] = lif
     return redirect('/')
+
+
+
+@app.route('/results')
+def res():
+    return render_template('otvetee.html',x_res=session['pravda'],p_res=session['nepravda'])
+
+
+@app.route('/logout')
+def zanovo():
+    session['nepravda']+=1
+    session['gamer']=False
+    return redirect('/')
+
 
 app.secret_key = 'isduf*&&(*(*&@jhhje893w92803834e'
 app.run()
+
